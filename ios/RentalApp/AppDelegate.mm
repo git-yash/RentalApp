d9826d86b57b0033016c7a84cfa530533ea32c90
git-yash <yashmittalshah@gmail.com>
2023-07-18 19:04:01 -0500

    added google map to explore screen with custom style and current location

--- ios/RentalApp/AppDelegate.mm
+++ ios/RentalApp/AppDelegate.mm
@@ -1,5 +1,6 @@
 #import "AppDelegate.h"
 #import <Firebase.h>
+#import <GoogleMaps/GoogleMaps.h>
 
 #import <React/RCTBundleURLProvider.h>
 
@@ -7,6 +8,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyBess9KnTOAUWkF1uLGWsCR6mdJM4VTvl0"];
   [FIRApp configure];
   self.moduleName = @"RentalApp";
   // You can add your custom initial props in the dictionary below.
--- screens/Explore/Explore.tsx
+++ screens/Explore/Explore.tsx
@@ -1,11 +1,17 @@
 import React, {useEffect, useState} from 'react';
-import {Alert, SafeAreaView} from 'react-native';
+import {Alert, View} from 'react-native';
 import LogInOrSignUp from '../../ModalScreens/LogInOrSignUp/LogInOrSignUp';
 import auth from '@react-native-firebase/auth';
-import Geolocation from '@react-native-community/geolocation';
+import Geolocation, {
+  GeolocationResponse,
+} from '@react-native-community/geolocation';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {Spinner} from 'native-base';
+import Colors from '../../assets/Colors';
 
 const Explore = (props: {navigation: any}) => {
   const [isModalVisible, setModalVisible] = useState(false);
+  const [canShowMap, setCanShowMap] = useState(false);
 
   useEffect(() => {
     if (!auth().currentUser) {
@@ -14,28 +20,46 @@ const Explore = (props: {navigation: any}) => {
       console.log(auth().currentUser?.email);
     }
 
-    getCurrentPosition();
+    getCurrentPosition().then(() => setCanShowMap(true));
   }, []);
 
-  const getCurrentPosition = () => {
-    Geolocation.getCurrentPosition(
+  const getCurrentPosition = async () => {
+    await Geolocation.getCurrentPosition(
       pos => {
-        setPosition(JSON.stringify(pos));
+        // setPosition(JSON.stringify(pos));
+        setPosition(pos);
+        console.log(pos.coords.latitude);
+        console.log(pos.coords.longitude);
       },
       error => Alert.alert('GetCurrentPosition Error', JSON.stringify(error)),
       {enableHighAccuracy: true},
     );
   };
 
-  const [position, setPosition] = useState<string | null>(null);
+  const [position, setPosition] = useState<GeolocationResponse>();
+  const mapStyle = require('../../assets/MapStyle.json');
 
   return (
-    <SafeAreaView>
+    <View style={{flex: 1}}>
       <LogInOrSignUp
         isModalVisible={isModalVisible}
         setIsModalVisible={setModalVisible}
       />
-    </SafeAreaView>
+      {canShowMap && (
+        <MapView
+          customMapStyle={mapStyle}
+          provider={PROVIDER_GOOGLE}
+          initialRegion={{
+            latitude: position?.coords.latitude as number, // San Diego Coords
+            longitude: position?.coords.longitude as number,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          style={{height: '100%', width: '100%'}}
+        />
+      )}
+      {!canShowMap && <Spinner color={Colors.green} />}
+    </View>
   );
 };
 
