fdfac16f477b71ab0db5f1a4e50f95fc94ccf16b
git-yash <yashmittalshah@gmail.com>
2023-08-28 17:56:24 -0500

    fixed zoom viewer picture issue

--- components/RentalDetailsImagesSlider/RentalDetailsImagesSlider.tsx
+++ components/RentalDetailsImagesSlider/RentalDetailsImagesSlider.tsx
@@ -1,26 +1,32 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Dimensions, Image, Modal, TouchableOpacity, View} from 'react-native';
 import Carousel from 'react-native-reanimated-carousel';
 import Colors from '../../assets/Colors';
 import ImageViewer from 'react-native-image-zoom-viewer';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {faMagnifyingGlassPlus} from '@fortawesome/free-solid-svg-icons';
+import {IImageInfo} from 'react-native-image-zoom-viewer/built/image-viewer.type';
 
 const RentalDetailsImagesSlider = (props: {picturePaths: string[]}) => {
   const windowWidth: number = Dimensions.get('window').width;
   const [currentIndex, setCurrentIndex] = useState<number>(0);
   const [isZoomViewerEnabled, setIsZoomViewerEnabled] = useState(false);
   const [canPressImage, setCanPressImage] = useState(true);
-  const images = [
-    {
-      url: props.picturePaths[0],
-      props: {},
-    },
-    {
-      url: props.picturePaths[1],
-      props: {},
-    },
-  ];
+  const [images, setImages] = useState<IImageInfo[]>([]);
+  // TODO: refactor this file
+
+  useEffect(() => {
+    const newImagesList = [];
+    for (let i = 0; i < props.picturePaths.length; i++) {
+      newImagesList.push({
+        url: props.picturePaths[i],
+        props: {},
+      });
+    }
+    setImages(newImagesList);
+
+    // converts picturePaths list to IImageInfo[] list for ImageViewer
+  }, []);
 
   return (
     <View>
@@ -38,6 +44,31 @@ const RentalDetailsImagesSlider = (props: {picturePaths: string[]}) => {
             width={windowWidth}
             height={200}
           />
+          <View
+            style={{
+              justifyContent: 'flex-end',
+            }}>
+            <TouchableOpacity
+              onPress={() => setIsZoomViewerEnabled(true)}
+              style={{
+                padding: 15,
+                position: 'absolute',
+                bottom: 5,
+                right: 5,
+                alignSelf: 'flex-end',
+                borderRadius: 100,
+                backgroundColor: 'black',
+                opacity: 0.5,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <FontAwesomeIcon
+                icon={faMagnifyingGlassPlus}
+                color={'white'}
+                size={25}
+              />
+            </TouchableOpacity>
+          </View>
         </TouchableOpacity>
       )}
       {props.picturePaths.length > 1 && (
@@ -88,10 +119,15 @@ const RentalDetailsImagesSlider = (props: {picturePaths: string[]}) => {
               />
             </TouchableOpacity>
           </View>
-          <View style={{flexDirection: 'row', padding: 10, height: 50}}>
+          <View
+            style={{
+              flexDirection: 'row',
+              padding: 10,
+              flexWrap: 'wrap',
+            }}>
             {props.picturePaths.map((item, index) => {
               return (
-                <View style={{paddingRight: 5}} key={index}>
+                <View style={{paddingRight: 5, paddingBottom: 10}} key={index}>
                   <TouchableOpacity onPress={() => setCurrentIndex(index)}>
                     <Image
                       source={{uri: item}}
